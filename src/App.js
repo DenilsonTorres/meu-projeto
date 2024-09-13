f0a170f23d4529dcad2169eaebd6b7e79d7233f3
Denilson Torres <dtlima2020@gmail.com>
2024-09-13 13:44:17 -0300

    feat: Aula 3 - ReactJs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld';
+
 
 function App() {
+  const name = "Denilson";
+  const newName = name.toUpperCase();
+
+  function sum(a, b) {
+    return a + b;
+  }
+
+  const url = "https://via.placeholder.com/150";
+
   return (
     <div className="App">
-      <h1> Olá React! </h1>
-      <p>Meu primeiro App</p>
+      <h1> Alterando JSX</h1>
+      <p>Olá, {newName}</p>
+      <p>Soma: {sum(5, 3)}</p>
+      <img src={url} alt="Minha Imagem" />
+      <HelloWorld/>
     </div>
   );
 }
